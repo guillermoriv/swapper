f6127cf144626e06ebe61f43292ed6e6bfa16ef0
capitanwesler <guillermo.campanudo@hotmail.com>
2021-05-10 00:41:08 +0100

    Quitting the comments in the SwapperV2 test

--- test/SwapperV2.test.js
+++ test/SwapperV2.test.js
@@ -45,116 +45,116 @@ describe('Testing the Swapper', () => {
     }
   }).timeout(30000000);
 
-  // it('change ETH for multiple tokens with % with decimals with balancer', async () => {
-  //   const porcents = [70.5 * 10, 29.5 * 10];
-  //   const tokens = [
-  //     '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
-  //     '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
-  //   ];
+  it('change ETH for multiple tokens with % with decimals with balancer', async () => {
+    const porcents = [70.5 * 10, 29.5 * 10];
+    const tokens = [
+      '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
+      '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
+    ];
 
-  //   for (let i = 0; i < tokens.length; i++) {
-  //     const transaction = await swapperV2.swapEthForTokensBalancer(
-  //       tokens[i],
-  //       porcents[i],
-  //       {
-  //         value: ethers.utils.parseEther('1'),
-  //       }
-  //     );
+    for (let i = 0; i < tokens.length; i++) {
+      const transaction = await swapperV2.swapEthForTokensBalancer(
+        tokens[i],
+        porcents[i],
+        {
+          value: ethers.utils.parseEther('1'),
+        }
+      );
 
-  //     const receipt = await transaction.wait();
-  //     console.log('Gas used for this transaction: ');
-  //     console.log(receipt.gasUsed.toString());
-  //   }
-  // }).timeout(30000000);
+      const receipt = await transaction.wait();
+      console.log('Gas used for this transaction: ');
+      console.log(receipt.gasUsed.toString());
+    }
+  }).timeout(30000000);
 
-  // it('change ETH for multiple tokens with uniswap', async () => {
-  //   const porcents = [40 * 10, 60 * 10];
-  //   const tokens = [
-  //     '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
-  //     '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
-  //   ];
+  it('change ETH for multiple tokens with uniswap', async () => {
+    const porcents = [40 * 10, 60 * 10];
+    const tokens = [
+      '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
+      '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
+    ];
 
-  //   for (let i = 0; i < tokens.length; i++) {
-  //     const transaction = await swapperV2.swapEthForToken(
-  //       tokens[i],
-  //       porcents[i],
-  //       {
-  //         value: ethers.utils.parseEther('1'),
-  //       }
-  //     );
+    for (let i = 0; i < tokens.length; i++) {
+      const transaction = await swapperV2.swapEthForToken(
+        tokens[i],
+        porcents[i],
+        {
+          value: ethers.utils.parseEther('1'),
+        }
+      );
 
-  //     const receipt = await transaction.wait();
-  //     console.log('Gas used for this transaction: ');
-  //     console.log(receipt.gasUsed.toString());
-  //   }
-  // }).timeout(30000000);
+      const receipt = await transaction.wait();
+      console.log('Gas used for this transaction: ');
+      console.log(receipt.gasUsed.toString());
+    }
+  }).timeout(30000000);
 
-  // it('change ETH for multiple tokens with % with decimals with uniswap', async () => {
-  //   const porcents = [70.5 * 10, 29.5 * 10];
-  //   const tokens = [
-  //     '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
-  //     '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
-  //   ];
+  it('change ETH for multiple tokens with % with decimals with uniswap', async () => {
+    const porcents = [70.5 * 10, 29.5 * 10];
+    const tokens = [
+      '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
+      '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
+    ];
 
-  //   for (let i = 0; i < tokens.length; i++) {
-  //     const transaction = await swapperV2.swapEthForToken(
-  //       tokens[i],
-  //       porcents[i],
-  //       {
-  //         value: ethers.utils.parseEther('1'),
-  //       }
-  //     );
+    for (let i = 0; i < tokens.length; i++) {
+      const transaction = await swapperV2.swapEthForToken(
+        tokens[i],
+        porcents[i],
+        {
+          value: ethers.utils.parseEther('1'),
+        }
+      );
 
-  //     const receipt = await transaction.wait();
-  //     console.log('Gas used for this transaction: ');
-  //     console.log(receipt.gasUsed.toString());
-  //   }
-  // }).timeout(30000000);
+      const receipt = await transaction.wait();
+      console.log('Gas used for this transaction: ');
+      console.log(receipt.gasUsed.toString());
+    }
+  }).timeout(30000000);
 
-  // it('choosing between dexes and who have best price', async () => {
-  //   const porcents = [40 * 10, 60 * 10];
-  //   const tokens = [
-  //     '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
-  //     '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
-  //   ];
+  it('choosing between dexes and who have best price', async () => {
+    const porcents = [40 * 10, 60 * 10];
+    const tokens = [
+      '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI Stablecoin
+      '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // Uniswap Token
+    ];
 
-  //   for (let i = 0; i < tokens.length; i++) {
-  //     const inchUrl = `https://api.1inch.exchange/v3.0/1/quote?fromTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&toTokenAddress=${
-  //       tokens[i]
-  //     }&amount=${ethers.utils.parseEther('1')}&protocols=UNISWAP_V2,BALANCER`;
+    for (let i = 0; i < tokens.length; i++) {
+      const inchUrl = `https://api.1inch.exchange/v3.0/1/quote?fromTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&toTokenAddress=${
+        tokens[i]
+      }&amount=${ethers.utils.parseEther('1')}&protocols=UNISWAP_V2,BALANCER`;
 
-  //     const result = await got(inchUrl);
-  //     const parsedResult = JSON.parse(result.body);
+      const result = await got(inchUrl);
+      const parsedResult = JSON.parse(result.body);
 
-  //     if (parsedResult.protocols[0][0][0].name === 'UNISWAP_V2') {
-  //       const transaction = await swapperV2.swapEthForToken(
-  //         tokens[i],
-  //         porcents[i],
-  //         {
-  //           value: ethers.utils.parseEther('1'),
-  //         }
-  //       );
+      if (parsedResult.protocols[0][0][0].name === 'UNISWAP_V2') {
+        const transaction = await swapperV2.swapEthForToken(
+          tokens[i],
+          porcents[i],
+          {
+            value: ethers.utils.parseEther('1'),
+          }
+        );
 
-  //       const receipt = await transaction.wait();
-  //       console.log('UNISWAP');
-  //       console.log('Gas used for this transaction: ');
-  //       console.log(receipt.gasUsed.toString());
-  //     }
+        const receipt = await transaction.wait();
+        console.log('UNISWAP');
+        console.log('Gas used for this transaction: ');
+        console.log(receipt.gasUsed.toString());
+      }
 
-  //     if (parsedResult.protocols[0][0][0].name === 'BALANCER') {
-  //       const transaction = await swapperV2.swapEthForTokensBalancer(
-  //         tokens[i],
-  //         porcents[i],
-  //         {
-  //           value: ethers.utils.parseEther('1'),
-  //         }
-  //       );
+      if (parsedResult.protocols[0][0][0].name === 'BALANCER') {
+        const transaction = await swapperV2.swapEthForTokensBalancer(
+          tokens[i],
+          porcents[i],
+          {
+            value: ethers.utils.parseEther('1'),
+          }
+        );
 
-  //       const receipt = await transaction.wait();
-  //       console.log('BALANCER');
-  //       console.log('Gas used for this transaction: ');
-  //       console.log(receipt.gasUsed.toString());
-  //     }
-  //   }
-  // }).timeout(30000000);
+        const receipt = await transaction.wait();
+        console.log('BALANCER');
+        console.log('Gas used for this transaction: ');
+        console.log(receipt.gasUsed.toString());
+      }
+    }
+  }).timeout(30000000);
 });
